2b1e95f810cecd25af015af247b2494f8e625af1
Andrew Tomko <tomand285@gmail.com>
2021-06-09 23:39:14 -0500

    Added a template for scp

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello Chia")
+const scp = require('node-scp')
+
+async function test() {
+  try {
+    const client = await scp({
+      host: 'your host',
+      port: 22,
+      username: 'username',
+      password: 'password',
+      // privateKey: fs.readFileSync('./key.pem'),
+      // passphrase: 'your key passphrase',
+    })
+    await client.uploadFile('./test.txt', '/workspace/test.txt');
+    const result = await client.stat('/server/path');
+    console.log(result)
+    client.close() // remember to close connection after you finish
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+test()
